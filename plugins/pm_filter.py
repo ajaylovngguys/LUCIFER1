03c6219ae42f34db2647ecdfecabc9d06bb6b139
ajaylovngguys <ajaylovingguys@gmail.com>
2022-02-15 21:11:49 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -708,7 +708,7 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
     else:
-        cap = f"↪️Requested: {search} \n {group_id} \n {user_id}  \n📌 Press The Down Buttons To Access The File \n📌 This Post Will Be Deleted After 10 Minutes "
+        cap = f"↪️Requested: {search} \n {user_id}  \n📌 Press The Down Buttons To Access The File \n📌 This Post Will Be Deleted After 10 Minutes "
     if imdb and imdb.get('poster'):
         try:
             await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024],

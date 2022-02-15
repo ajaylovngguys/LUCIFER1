bb87aef1fcf26398512d7cef7e5dce8e30fe34e4
ajaylovngguys <ajaylovingguys@gmail.com>
2022-02-15 19:20:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Akbotmakersnew/LUCIFER.git /LUCIFER
+  git clone https://github.com/ajaylovngguys/LUCIFER1.git /LUCIFER1
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /LUCIFER
+  git clone $UPSTREAM_REPO /LUCIFER1
 fi
-cd /LUCIFER
+cd /LUCIFER1
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
